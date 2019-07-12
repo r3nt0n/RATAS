1b39ed4fa5d6ea3ef9f7e6223e32427a9247312a
R3nt0n <bvegainf@gmail.com>
2019-07-12 19:34:47 +0200

    Add files via upload

---
+++ client/lib/cmd_help.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# RATAS [commandHelp]
+
+
+##########################################################################
+# MANUAL DE COMANDOS
+# Entra comando y devuelve la entrada del manual relacionada
+##########################################################################
+def manual(command):
+
+    if command == 'help':
+        output = '''
+                           *** COMANDOS ESPECIALES ***
+    -------------------------------------------------------------------------
+        HELP:           Muestra la lista de comandos especiales
+        CLEAR:          Limpiar pantalla
+        PWD:            Muestra el directorio en el que te encuentras
+        FTP:            Conectar a un servidor FTP
+        GETFILE:        Descargar un fichero del servidor en esta máquina
+        SENDFILE:       Enviar un fichero de esta máquina al servidor
+        START:          Ejecutar un archivo en el equipo servidor
+        READ:           Muestra el contenido de un fichero en la consola
+        SHUTDOWN:       Apagar el servidor
+        REBOOT:         Reiniciar el servidor
+        EXIT:           Salir de la consola de comandos
+    -------------------------------------------------------------------------\n
+    *Para más info sobre un comando especial (descripción, modificadores...):
+     help [comando]\n'''
+
+##########################################################################
+
+    elif command.startswith('help ftp'):  # AYUDA DE FTP
+        output = '''
+Este comando sirve para realizar una conexion a un servidor FTP.\n
+Tras ejecutar el comando, se pedira la IP del host, el usuario y la contraseña.\n
+La sintaxis es [ftp] ENTER [ip-host] ENTER [user] ENTER [password]\n'''
+
+##########################################################################
+
+    elif command.startswith('help start'):  # AYUDA DE START
+        output = '''
+Este comando sirve para ejecutar un archivo en el equipo del servidor.\n
+La sintaxis es: [start] [nombre-del-archivo]\n
+Ejemplo: start client.py\n'''
+
+##########################################################################
+
+    elif command.startswith('help:&:read'):  # AYUDA DE READ
+        output = '''
+Este comando sirve para mostrar por la consola el contenido de un fichero alojado en el equipo servidor.\n
+La sintaxis es: [read] [nombre-del-archivo]\n
+Ejemplo: read texto.txt\n'''
+
+##########################################################################
+
+    elif command.startswith('help:&:clear'):  # AYUDA DE CLEAR
+        output = '''
+Este comando sirve para limpiar la pantalla.\n'''
+
+##########################################################################
+
+    elif command.startswith('help:&:getfile'):
+        output = '''
+Este comando sirve para descargar ficheros que estén alojados en el servidor.
+\nLa sintaxis es: getfile [rutaOrigen] [rutaDestino]\n
+- La ruta de origen hace referencia a la ruta donde se encuentra alojado el fichero en el servidor.
+- La ruta de destino hace referencia a la ruta donde se almacenará el fichero en
+  la máquina cliente.
+- Las rutas pueden ser absolutas o relativas.
+\nEjemplo: getfile D:/Documents/fichero.exe D:/Downloads/ficheroDescargado.exe'''
+
+##########################################################################
+
+    elif command.startswith('help:&:sendfile'):
+        output = '''
+Este comando sirve para enviar ficheros que estén alojados en el cliente hacia
+la máquina del servidor.\n
+La sintaxis es: sendfile [rutaOrigen] [rutaDestino]\n
+- La ruta de origen hace referencia a la ruta donde se encuentra alojado el
+  fichero en esta máquina.
+- La ruta de destino hace referencia a la ruta donde se almacenará el fichero en
+  la máquina servidor.
+- Las rutas pueden ser absolutas o relativas.\n
+Ejemplo: getfile D:/Documents/fichero.exe D:/Downloads/ficheroDescargado.exe'''
+
+##########################################################################
+
+    elif command.startswith('help:&:shutdown'):  # AYUDA DE SHUTDOWN
+            output = '''
+Este comando sirve para apagar el servidor de forma inmediata, forzando el cierre de aplicaciones.
+\nLa sintaxis es: [shutdown]\n'''
+
+##########################################################################
+
+    elif command.startswith('help:&:reboot'):  # AYUDA DE REBOOT
+        output = '''
+Este comando sirve para reiniciar el servidor de forma inmediata, forzando el cierre de aplicaciones.
+\nLa sintaxis es: [reboot]\n'''
+
+##########################################################################
+
+    elif command.startswith('help:&:exit'):  # AYUDA DE EXIT
+        output = 'Este comando sirve para cerrar la consola.\n'
+
+    else:
+        output = 'No existe una entrada en el manual para ese comando.\n'
+
+##########################################################################
+
+    return output
+##########################################################################
+
+
+##########################################################################
+# MANUAL DE COMANDOS FTP
+# Entra command, devuelve output
+##########################################################################
+def manualFTP(comftp):
+
+    if comftp == 'help':
+        output = '''
+                             *** COMANDOS FTP ***
+    ---------------------------------------------------------------------
+    HELP:           Muestra la lista de comandos que se pueden utilizar
+    CLEAR:          Limpiar pantalla
+    PWD:            Muestra la ruta del directorio actual
+    CD:             Cambia de directorio
+    LS:             Muestra el contenido del directorio actual
+    MKDIR:          Crear un directorio o árbol de directorios
+    RMDIR:          Eliminar un directorio
+    UP:             Subir un fichero al servidor FTP
+    DOWN:           Descargar un fichero del servidor FTP
+    BYE:            Cerrar la conexión FTP y regresar a la consola remota
+    ---------------------------------------------------------------------\n
+   **Para obtener ayuda sobre algún comando:    help comando**\n
+   **NOTA: Para ejecutar comandos locales, anteponer ! al comando.
+           Ejemplo: !pwd\n\n'''
+
+    elif comftp.startswith('shortHelp'):
+        output = '''
+Para conocer los comandos FTP disponibles escribe help.
+Para ejecutar comandos locales, anteponer ! al comando.'''
+
+    elif comftp.startswith('help:&:clear'):  # AYUDA DE CLEAR
+        output = '''
+Este comando sirve para limpiar la pantalla.\n'''
+
+##########################################################################
+
+    elif comftp.startswith('help pwd'):  # AYUDA DE PWD
+        output = '''
+Este comando sirve para mostrar la ruta del directorio en el que te encuentras.\n'''
+
+##########################################################################
+
+    elif comftp.startswith('help !pwd'):  # AYUDA DE LPWD
+        output = '''
+Este comando sirve para mostrar la ruta del directorio local en el que te encuentras.\n'''
+
+##########################################################################
+
+    elif comftp.startswith('help cd'):  # AYUDA DE CD
+        output = '''
+Este comando sirve para cambiar de directorio.\n
+La sintaxis es: [cd] [ruta]\n
+Ejemplo: cd pages\n'''
+
+##########################################################################
+
+    elif comftp.startswith('help !cd'):  # AYUDA DE LCD
+        output = '''
+Este comando sirve para cambiar el directorio local.\n
+La sintaxis es: [lcd] [ruta]\n
+Ejemplo: lcd pages\n'''
+
+##########################################################################
+
+    elif comftp.startswith('help ls'):  # AYUDA DE LS
+        output = '''
+Este comando sirve para listar el contenido del directorio actual.'''
+
+##########################################################################
+
+    elif comftp.startswith('help !ls'):  # AYUDA DE LLS
+        output = '''
+Este comando sirve para listar el contenido del directorio actual en el equipo local.'''
+
+##########################################################################
+
+    elif comftp.startswith('help:&:mkdir'):  # AYUDA DE MKDIR
+        output = '''
+Este comando sirve para crear directorios.\n
+La sintaxis es: [mkdir] [ruta]\n
+Ejemplo: mkdir carpeta'''
+
+##########################################################################
+
+    elif comftp.startswith('help rmdir'):  # AYUDA DE RMDIR
+        output = '''
+Este comando sirve para eliminar directorios.\n
+La sintaxis es: [rmdir] [ruta]\n
+Ejemplo: rmdir carpeta\n'''
+
+##########################################################################
+
+    elif comftp.startswith('help up'):  # AYUDA DE UP
+        output = '''
+Este comando sirve para subir un fichero al host FTP desde el equipo servidor.\n
+La sintaxis es: [up] [ruta]\n
+Ejemplo: up imagen.png'''
+
+##########################################################################
+
+    elif comftp.startswith('help down'):  # AYUDA DE DOWN
+        output = '''
+Este comando sirve para descargar un fichero desde el host FTP al equipo servidor.\n
+La sintaxis es: [down] [ruta]\n
+Ejemplo: down imagen.png'''
+
+##########################################################################
+
+    else:
+        output = 'No existe una entrada en el manual para ese comando.\n'
+
+    return output
+##########################################################################
---
+++ client/lib/shell_cli.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# RATAS [shell_client_module]
+
+import os
+from getpass import getpass
+
+from color import color
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+from cmd_help import manual, manualFTP  # Manual de los comandos
+
+
+class Shell:
+    def __init__(self, conn):
+        self.conn = conn
+        self.shellFTP = False
+        self.ipServer = None
+        self.pwd = None
+
+    # ACTUALIZAR EL PROMPT
+    ############################################################################
+    def updatePrompt(self, output):
+        if output.startswith(':FTP:'):
+            self.shellFTP = True
+            prompt = 'ftp > '
+        else:
+            self.shellFTP = False
+            prompt = '{}root@{}{}:{}{}{}# '.format(color.RED + color.BOLD, self.ipServer, color.END, color.BLUE + color.BOLD, self.pwd, color.END)
+        return prompt
+
+    # FORMATEO DE LA ENTRADA
+    ############################################################################
+    def formatInput(self, command):
+        command = command.strip(' ')
+        command = command.replace('\ ', ':SP:')  # Sustituir espacios escapados
+        command = command.replace(' ', ':&:')    # Separador de argumentos
+        command = command.replace(':SP:', '\ ')   # Sustituir espacios escapados
+        return command
+
+    # FORMATEO DE LA SALIDA
+    ############################################################################
+    def formatOutput(self, output):
+        if ':CWD:' in output:
+            self.pwd = output.split(':CWD:')[-1:][0]
+            output = output.split(':CWD:')[:-1][0]
+        if output.startswith(':FTP:'):
+            output = output[5:]
+        if output.startswith(':ERR:'):
+            output = '{}[!]{} ERROR: {}'.format(color.RED,color.END, output[5:])
+        return output
+
+    # LIMPIAR LA PANTALLA
+    ############################################################################
+    def clear(self):
+        os.system(['clear', 'cls'][os.name == 'nt'])
+
+    # COMPROBAR SI UNA VARIABLE ESTA VACIA
+    ############################################################################
+    def isEmpty(self, variable):
+        empty = False
+        if len(str(variable)) == 0:
+            empty = True
+        return empty
+
+    # RECEPCIÓN DE FICHEROS POR EL SOCKET
+    ############################################################################
+    def getFile(self, destin):
+        exists = self.conn.receive()
+        if exists == ':NOEXISTS:':
+                output = ':ERR:El fichero no existe'
+        else:
+            while True:
+                self.conn.send(':SYNC:')
+                bytesEncoded = self.conn.receive()
+
+                if bytesEncoded == ':ENDFILE:':
+                    output = '{}[+]{} La descarga se ha completado'.format(color.GREEN, color.END)
+                    self.conn.receive() # Sincronización
+                    break
+                else:
+                    try:
+                        bytesDecoded = urlsafe_b64decode(bytesEncoded)
+                        with open(destin, 'ab') as fileToSave:
+                            fileToSave.write(bytesDecoded)
+
+                    except:
+                        output = ':ERR:Ha habido un error en la transferencia'
+                        break
+        return output
+
+    # ENVÍO DE FICHEROS POR EL SOCKET
+    ############################################################################
+    def sendFile(self, fileToSend):
+        output = ''
+        if os.path.exists(fileToSend):
+            self.conn.send(':EXISTS:')
+            with open(fileToSend, 'rb') as f:
+                data = f.readlines()
+            for line in data:
+                lineEncoded = urlsafe_b64encode(line)   # codifica el fichero
+                imReady = self.conn.receive()
+                if imReady == ':SYNC:':
+                    self.conn.send(lineEncoded)
+                else:  # recibe ERROR
+                    output = ':ERR:Ha habido un error en la transferencia'
+
+            self.conn.send(':ENDFILE:')
+            imReady = self.conn.receive() # Sincronización
+
+            if imReady == ':SYNC:':
+                output = '{}[+]{} La descarga se ha completado'.format(color.GREEN, color.END)
+
+        else:
+            self.conn.send(':NOEXISTS:')
+            output = ':ERR:El fichero no existe'
+
+        return output
+
+    # EJECUCIÓN DE COMANDOS
+    ############################################################################
+    def exeRemote(self, command):
+        command = self.formatInput(command)
+        output = ''
+
+        ########################################################################
+        if command == 'exit':            # Cierra la conexión
+            self.conn.send(command)
+            self.conn.close()
+            output = ':CLOSE:'
+        ########################################################################
+        elif command.startswith('help'):   # Muestra la ayuda de un comando
+            output = manual(command)
+        ########################################################################
+        elif command.startswith('clear'):  # Limpia la pantalla
+            self.clear()
+        ########################################################################
+        elif command.startswith('ftp'):  # Conexion con servidor FTP
+
+            if len(command.split(':&:')) != 2:
+                output = ':ERR:No puedes dejar el campo del host en blanco'
+            else:
+                user = raw_input('\nIntroduce el nombre de usuario >>> ')
+                password = getpass('\nIntroduce la contraseña >>> ')
+                if not self.isEmpty(user):
+                    command += ':&:' + user + ':&:' + password
+                self.conn.send(command)
+                output = self.conn.receive()
+                if output.startswith(':FTP:'):
+                    self.clear()
+                    output = output + '\n' + manualFTP('shortHelp')
+        ########################################################################
+        elif command.startswith('getfile'):
+            inputData = command.split(':&:')
+            if len(inputData) != 3:
+                output = ':ERR:Número de argumentos incorrecto.\n' + \
+                         'Escribe help getfile para conocer la sintaxis.\n'
+            else:
+                command = inputData[0]
+                fileToGet = inputData[1]
+                destin = inputData[2]
+                self.conn.send(command + ':&:' + fileToGet)
+                output = self.getFile(destin)
+        ########################################################################
+        elif command.startswith('sendfile'):
+            inputData = command.split(':&:')
+            if len(inputData) != 3:
+                output = ':ERR:Número de argumentos incorrecto.\n' + \
+                         'Escribe help sendfile para conocer la sintaxis.\n'
+            else:
+                command = inputData[0]
+                fileToSend = inputData[1]
+                destin = inputData[2]
+                self.conn.send(command + ':&:' + destin)
+                output = self.sendFile(fileToSend)
+        ########################################################################
+        elif self.isEmpty(command):
+            output = ':ERR: No has introducido ningun comando'
+        ########################################################################
+        else:
+            self.conn.send(command)
+            output = self.conn.receive()
+        ########################################################################
+        return output
+
+    # EJECUCIÓN DE COMANDOS FTP
+    ############################################################################
+    def exeRemoteFTP(self, comftp):
+        outputFTP = ":FTP:"
+        if comftp.startswith('help'):
+            outputFTP += manualFTP(comftp)
+        elif comftp.startswith('clear'):  # Limpiar la pantalla
+            self.clear()
+        elif self.isEmpty(comftp):
+            outputFTP += ':ERR:No has introducido ningun comando'
+        else:  # Enviar comando FTP
+            self.conn.send(':FTP:' + comftp)
+            outputFTP = self.conn.receive()
+        return outputFTP
+
+    # ARRANCAR LA SHELL
+    ############################################################################
+    def start(self):
+        initial_info = (self.conn.receive()).split(':CWD:')
+        self.ipServer = initial_info[0]
+        self.pwd = initial_info[1]
+        self.clear()
+        print manual('help')
+        output = ''
+
+        while True:
+            try:
+                prompt = self.updatePrompt(output)
+                command = raw_input(prompt)
+
+                if self.shellFTP:
+                    output = self.exeRemoteFTP(command)
+                else:
+                    output = self.exeRemote(command)
+
+                if output == ':CLOSE:':
+                    break
+                else:
+                    print self.formatOutput(output)
+
+            except:
+                self.conn.send('exit')
+                self.conn.close()
+                break
+
+
+# Ejemplo de uso:
+if __name__ == '__main__':
+    from connection import Connection
+    conn = Connection('localhost', 5555)
+    conn.listen()
+    shell = Shell(conn)
+    shell.start()

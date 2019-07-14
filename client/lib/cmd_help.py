40a7d7cbd9375ae7460107ec91107dee93b46495
R3nt0n <bvegainf@gmail.com>
2019-07-14 16:33:18 +0200

    Update cmd_help.py

--- client/lib/cmd_help.py
+++ client/lib/cmd_help.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# RATAS [commandHelp]
+# RATAS [commandHelp] (https://www.github.com/R3nt0n/ratas)
+# R3nt0n (https://www.github.com/R3nt0n)
 
 
 ##########################################################################

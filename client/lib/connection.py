06526a7ba636a9708c7ed7914f88acaf4e364e70
R3nt0n <bvegainf@gmail.com>
2019-07-14 16:33:59 +0200

    Update connection.py

--- client/lib/connection.py
+++ client/lib/connection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# RATAS [connection_module]
+# RATAS [connection_module] (https://www.github.com/R3nt0n/ratas)
+# R3nt0n (https://www.github.com/R3nt0n)
 
 import socket
 from requests import get

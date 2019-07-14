4ac701c99e077d3154efa83b30b4bdfa1c7ad757
R3nt0n <bvegainf@gmail.com>
2019-07-14 16:34:09 +0200

    Update keyring.py

--- client/lib/keyring.py
+++ client/lib/keyring.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# RATAS [keyring_module]
+# RATAS [keyring_module] (https://www.github.com/R3nt0n/ratas)
+# R3nt0n (https://www.github.com/R3nt0n)
 
 from base64 import b64encode, b64decode
 
